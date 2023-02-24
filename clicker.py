6b5e373bf8fd940e8a28026ad061ff54de5c560c
G-Hame <ZorinG687@gmail.com>
2023-02-25 00:20:58 +0300

    Initial commit

---
+++ clicker.py
@@ -0,0 +1,28 @@
+import time
+import threading
+from pynput.mouse import Controller, Button
+from pynput.keyboard import Listener, KeyCode
+
+toggle_key = KeyCode(char="m")
+
+clicking = False
+mouse = Controller()
+
+
+def clicker():
+    while True:
+        if clicking:
+            mouse.click(Button.left, 1)
+        time.sleep(1)
+
+def toggle_event(key):
+    if key == toggle_key:
+        global clicking
+        clicking = not clicking
+
+
+click_thread = threading.Thread(target=clicker)
+click_thread.start()
+
+with Listener(on_press=toggle_event) as Listener:
+    Listener.join()
